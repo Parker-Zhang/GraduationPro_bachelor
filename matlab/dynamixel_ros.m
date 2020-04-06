63dbc91ad12289a544aacdbf0bf127a81fdcd96c
Parker Zhang <1742356236@qq.com>
2020-04-06 16:55:40 +0800

    dataUpdate

--- matlab/dynamixel_ros.m
+++ matlab/dynamixel_ros.m
@@ -29,8 +29,8 @@ t=1:1:length(radian);
 figure(1),
 plot(t,radian')
 
-%% 05 .txt 绘制K=0.03 K=0.04 01.txt K=0.05
-% 绘制K = 0.03；曲线
+%% 05 .txt Kp=0.03 Kp=0.04 01.txt Kp=0.05
+% K = 0.03
 filename='data/05.txt';
 [time,radian,velocity,current]=textread(filename,'%*s %f %*s %d %*s %f %*s %f','delimiter',':');
 t=1:1:length(radian);
@@ -58,9 +58,8 @@ t=1:1:length(radian);
 plot(0.1*t,y3,'linewidth',1.1); 
 axis([7, 14, 0 1400]) 
 set(gca, 'linewidth', 1.1, 'fontsize', 16, 'fontname', 'times') 
-xlabel('时间(s)','fontname','宋体') 
-% ylabel('Displacement ') 
-ylabel('位移','fontname','宋体')
+xlabel('Time(s)') ;
+ylabel('Displacement ') 
 legend('desired','Kp=0.03','Kp=0.04','Kp=0.05')
 %% 09.txt p=0.03 d=0.05;p=0.04 d=0.05
 filename='data/09.txt';
@@ -82,9 +81,8 @@ t=1:length(y2);
 plot(0.1*t,y2,'linewidth',1.1)
 axis([6, 14, 0 1400]) 
 set(gca, 'linewidth', 1.1, 'fontsize', 16, 'fontname', 'times') 
-xlabel('时间(s)','fontname','宋体') 
-% ylabel('Displacement ') 
-ylabel('位移','fontname','宋体')
+xlabel('Time'); 
+ylabel('Displacement ') ;
 legend('desired','Kp=0.03,Kd=0.05','Kp=0.04,Kd=0.05')
 
 
@@ -92,25 +90,74 @@ legend('desired','Kp=0.03,Kd=0.05','Kp=0.04,Kd=0.05')
 % uint: current:2.69[mA]  velocity:0.229[rev/min]  position:1[pulse] 0.088掳/Value
 clc
 clear
-filename='data/11.txt';
+filename='data/state07.txt';
 [time,radian1,velocity1,current1,radian2,velocity2,current2,radian0,velocity0,current0]...
 =textread(filename,'%f %*s %*d %d %d %d %*s %*d %d %d %d %*s %*d %d %d %d','delimiter',',');
+filename2='data/d_tra07.txt';
+[time,dp1,dp2,dp0]=textread(filename2,'%f %*d %*d %*d %d %d %d','delimiter',',');
 t=1:1:length(radian1);
+t1=1:1:length(dp1);
 figure(100),
-plot(t,radian1');
+plot(t,radian1', 'linewidth', 1.1,'color','#0072BD');
 hold on
-plot(t,radian2');
-plot(t,radian0');
+plot(t,radian2', 'linewidth', 1.1,'color','#D95319');
+plot(t,radian0', 'linewidth', 1.1,'color','#EDB120');
+plot(t1,dp1', 'linewidth', 1.1,'linestyle','--','color','#0072BD');
+plot(t1,dp2', 'linewidth', 1.1,'linestyle','--','color','#D95319');
+plot(t1,dp0', 'linewidth', 1.1,'linestyle','--','color','#EDB120');
+set(gca, 'linewidth', 1.1, 'fontsize', 16, 'fontname', 'times') 
+xlabel('Time'); 
+ylabel('Radian ') ;
+legend('id=1','id=2','id=0','id=1','id=2','id=0');
 figure(200),
-plot(t,velocity1');
+plot(t,velocity1', 'linewidth', 1.1);
 hold on
-plot(t,velocity2');
-plot(t,velocity0');
+plot(t,velocity2', 'linewidth', 1.1);
+plot(t,velocity0', 'linewidth', 1.1);
+set(gca, 'linewidth', 1.1, 'fontsize', 16, 'fontname', 'times') ;
+xlabel('Time'); 
+ylabel('Velocity ') ;
+legend('id=1','id=2','id=0');
 figure(300),
-plot(t,current1');
+plot(t,current1', 'linewidth', 1.1);
 hold on
-plot(t,current2');
-plot(t,current0');
+plot(t,current2', 'linewidth', 1.1);
+plot(t,current0', 'linewidth', 1.1);
+set(gca, 'linewidth', 1.1, 'fontsize', 16, 'fontname', 'times') 
+xlabel('Time'); 
+ylabel('Current ') ;
+legend('id=1','id=2','id=0');
+figure(400),
+plot3(dp0,dp1,dp2,'marker','*','color','#0072BD');
+hold on,
+grid on,
+plot3(radian0,radian1,radian2,'marker','*','color','#D95319');
+set(gca, 'linewidth', 1.1, 'fontsize', 16, 'fontname', 'times') 
+xlabel('id0'); 
+ylabel('id1 ') ;
+zlabel('id2');
+legend('desired\_pos','actual\_pos');
+%% desired trajectory
+filename2='data/d_tra04.txt';
+[time,dp1,dp2,dp0]=textread(filename2,'%f %*d %*d %*d %d %d %d','delimiter',',');
+t=1:1:length(time);
+figure(400),
+plot(t,dp1', 'linewidth', 1.1,'linestyle','--','color','#0072BD');
+hold on
+plot(t,dp2', 'linewidth', 1.1,'linestyle','--','color','#D95319');
+plot(t,dp0', 'linewidth', 1.1,'linestyle','--','color','#EDB120');
+%% 3DOF count
+figure(10),
+plot3(dp0,dp1,dp2,'marker','*','color','#0072BD');
+hold on,
+grid on,
+plot3(radian0,radian1,radian2,'marker','*','color','#D95319');
+set(gca, 'linewidth', 1.1, 'fontsize', 16, 'fontname', 'times') 
+xlabel('id0'); 
+ylabel('id1 ') ;
+zlabel('id2');
+legend('desired\_pos','actual\_pos');
+
 %% output trajectory yaml file
 clc
 fid = fopen('motorTra.yaml','w');
@@ -120,7 +167,8 @@ wayPointNum = len;
 thetaData=motorTheta(1:wayPointNum,1:motorNum);
 rowIndex = 1;
 colIndex = 1;
-startTime = 0;
+startTime = 0.2;
+dTime = 0.01;
 fprintf(fid,'joint:\n');
 fprintf(fid,'  names: [zero,first,second]\n');
 fprintf(fid,'trajectory:\n');
@@ -141,7 +189,7 @@ for i=1:wayPointNum
     fprintf(fid,'%f]\n',thetaData(rowIndex,colIndex));
     rowIndex=rowIndex+1;
     colIndex = 1;
-    startTime = startTime+0.2;
+    startTime = startTime+dTime;
     fprintf(fid,'    time_from_start: %f\n',startTime);
 end
 fclose(fid);
